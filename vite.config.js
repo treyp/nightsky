0e0dda601f097c650ed66dc324922da485e766f0
Trey Philips <treyjp@gmail.com>
2023-03-07 23:02:56 -0600

    Remove most scaffolding boilerplate content

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bluesky/'
-})
+  base: "/bluesky/",
+  publicDir: false,
+});
