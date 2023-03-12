1f18f357773b2e43c576557a692eb6a7bf50d82c
Trey Philips <treyjp@gmail.com>
2023-03-12 05:05:55 -0500

    Align main content to top on desktop

--- src/sections/Content.jsx
+++ src/sections/Content.jsx
@@ -2,7 +2,7 @@ import MobileNavBar from "./MobileNavBar";
 
 export default function Content({ children }) {
   return (
-    <div className="drawer-content flex flex-col lg:justify-center items-center px-4 lg:pr-0">
+    <div className="drawer-content flex flex-col items-center px-4 lg:pr-0 lg:pt-4">
       <MobileNavBar />
       {children}
     </div>
