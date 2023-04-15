33309e1563022412267f9f52c7c1e0b782ac69fc
Trey Philips <treyjp@gmail.com>
2023-04-15 03:39:52 -0500

    Fix duplicate key when item appears twice in feed due to repost

--- src/sections/Feed.jsx
+++ src/sections/Feed.jsx
@@ -8,7 +8,9 @@ export default function Feed({ feed }) {
         feed.map((feedItem) => (
           <div
             className="border-b border-gray-800 my-2 px-4"
-            key={feedItem.post?.cid}
+            key={`${feedItem.post?.cid}${feedItem.reason?.$type || ""}${
+              feedItem.reason?.by?.did || ""
+            }`}
           >
             {feedItem.reply?.parent && (
               <Post post={feedItem.reply.parent} isParent />
