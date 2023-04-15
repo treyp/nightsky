119ab0f4e1008415824f44b3b09be14d604c6af6
Trey Philips <treyjp@gmail.com>
2023-04-15 15:32:41 -0500

    Post interactions: link to replies, debug on repost and like

--- src/sections/PostInteractions.jsx
+++ src/sections/PostInteractions.jsx
@@ -3,22 +3,36 @@ import {
   ArrowPathRoundedSquareIcon,
   HeartIcon,
 } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 export default function PostInteractions({ post }) {
+  const recordId = post.uri.split("/").at(-1);
   return (
-    <div className="text-gray-400 mt-2 text-sm grid grid-cols-3 max-w-xs">
-      <div>
+    <div className="text-gray-400 mt-2 text-sm grid grid-cols-3 gap-8 w-60">
+      <Link to={`/profile/${post.author?.handle}/post/${recordId}`}>
         <ChatBubbleBottomCenterIcon className="h-4 w-4 inline-block stroke-2 mr-2" />
         {post.replyCount || ""}
-      </div>
-      <div>
+      </Link>
+      <a
+        href="#"
+        onClick={(e) => {
+          e.preventDefault();
+          console.log(post);
+        }}
+      >
         <ArrowPathRoundedSquareIcon className="h-4 w-4 inline-block stroke-2 mr-2" />
         {post.repostCount || ""}
-      </div>
-      <div>
+      </a>
+      <a
+        href="#"
+        onClick={(e) => {
+          e.preventDefault();
+          console.log(post);
+        }}
+      >
         <HeartIcon className="h-4 w-4 inline-block stroke-2 mr-2" />
         {post.upvoteCount || ""}
-      </div>
+      </a>
     </div>
   );
 }
